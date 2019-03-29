9042fd6f70c90b601d220f0f12a0dc5df87d48b1
hyu103 <38918700+hyu103@users.noreply.github.com>
2019-03-28 21:19:38 -0400

    add account class

--- app.js
+++ app.js
@@ -6,10 +6,47 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+class Account {
+    constructor(firstName, lastName, phoneNumber, email, password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+    }
+}
+
+app.post("/newAccount", function(req, res){
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const phoneNumber = req.body.phoneNumber;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    let acct = new Account(firstName, lastName, phoneNumber, email, password);
+    
+    res.redirect("/");
+});
+
+app.post("/login", function(req, res) {
+    const email = req.body.emailInput;
+    const password = req.body.passwordInput;
+    const loginSuccess = false;
+    if(email == acct.email && password == acct.password) {
+        loginSuccess = true;
+    }
+    console.log(loginSuccess);
+})
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/newAccount", function(req, res){
+    res.sendFile(__dirname + "/newAccount.html");
+})
+
 app.listen(3000, function() {
     console.log("Server running on port 3000");
 });
+
